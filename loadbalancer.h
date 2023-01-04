f6cba59bf6508417cd29b980605ec9191bd68d24
carter-ho <98185752+carter-ho@users.noreply.github.com>
2023-01-04 09:15:32 +0800

    Initialize Repo

---
+++ loadbalancer.h
@@ -0,0 +1,13 @@
+#ifndef LOADBALANCER_H
+#define LOADBALANCER_H
+
+class Loadbalancer
+{
+public:
+
+private:
+
+};
+
+
+#endif
---
+++ webserver.h
@@ -0,0 +1,12 @@
+#ifndef WEBSERVER_H
+#define WEBSERVER_H
+
+class Webserver
+{
+public:
+
+private:
+
+};
+
+#endif
