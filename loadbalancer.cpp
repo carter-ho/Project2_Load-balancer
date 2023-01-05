d788eb1495dd2e716aa1b65fbd60ea07ee02f5ba
rgovat <rkg0094@tamu.edu>
2023-01-05 19:29:07 +0800

    added comments to all cpp files

--- loadbalancer.cpp
+++ loadbalancer.cpp
@@ -5,7 +5,9 @@
 using namespace std;
 
 Loadbalancer::Loadbalancer(int queue_size, int max_time, float probability){
-    max_cycles = max_time;
+    
+	// Initializes a Loadbalancer with all of its parameterized attributes and created all requests to add to the queue
+	max_cycles = max_time;
     new_request_probability = probability;
     for(int i = 0; i < queue_size; i++){
         Request r;
@@ -14,13 +16,16 @@ Loadbalancer::Loadbalancer(int queue_size, int max_time, float probability){
 }
 
 Request Loadbalancer::getRequest(){
+	
+	// Stores and pops the request at the front of the queue
     Request r = request_queue.front();
     request_queue.pop();
     return r;
 }
 
 bool Loadbalancer::isNotEmpty()
-{ 
+{
+	// Returns whether the queue is empty or not
     if(request_queue.empty())
     {
         return false;
@@ -29,11 +34,14 @@ bool Loadbalancer::isNotEmpty()
 }
 
 void Loadbalancer::addRequest(){
-    std::random_device rd;
+    
+	// Creates a random number to test against probability threshold
+	std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(1, 100);
     int random_num = dis(gen);
 
+	// Determines if random number meets probability threshold and adds a new request if threshold is met
     if(random_num > (100 - new_request_probability)){
         Request f;
         request_queue.push(f);
