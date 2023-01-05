328b8599a7958d438158b4abdd07796ca7a21a6d
rgovat <rkg0094@tamu.edu>
2023-01-05 21:12:20 +0800

    added 3rd / to comments

--- loadbalancer.cpp
+++ loadbalancer.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 Loadbalancer::Loadbalancer(int queue_size, int max_time, float probability){
     
-	// Initializes a Loadbalancer with all of its parameterized attributes and created all requests to add to the queue
+	/// Initializes a Loadbalancer with all of its parameterized attributes and created all requests to add to the queue
 	max_cycles = max_time;
     new_request_probability = probability;
     for(int i = 0; i < queue_size; i++){
@@ -17,7 +17,7 @@ Loadbalancer::Loadbalancer(int queue_size, int max_time, float probability){
 
 Request Loadbalancer::getRequest(){
 	
-	// Stores and pops the request at the front of the queue
+	/// Stores and pops the request at the front of the queue
     Request r = request_queue.front();
     request_queue.pop();
     return r;
@@ -25,7 +25,7 @@ Request Loadbalancer::getRequest(){
 
 bool Loadbalancer::isNotEmpty()
 {
-	// Returns whether the queue is empty or not
+	/// Returns whether the queue is empty or not
     if(request_queue.empty())
     {
         return false;
@@ -35,13 +35,13 @@ bool Loadbalancer::isNotEmpty()
 
 bool Loadbalancer::addRequest(){
     
-	// Creates a random number to test against probability threshold
+	/// Creates a random number to test against probability threshold
 	std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(1, 100);
     int random_num = dis(gen);
 
-	// Determines if random number meets probability threshold and adds a new request if threshold is met
+	/// Determines if random number meets probability threshold and adds a new request if threshold is met
     if(random_num > (100 - new_request_probability)){
         Request f;
         request_queue.push(f);
--- request.cpp
+++ request.cpp
@@ -6,11 +6,11 @@
 using std::cin, std::cout, std::endl, std::string;
 
 Request::Request(){
-    ip_in = generate_ipv4_address(); // Calls random funtion to create a random ip in address
-    ip_out = generate_ipv4_address(); // Calls random funtion to create a random ip out address
+    ip_in = generate_ipv4_address(); /// Calls random funtion to create a random ip in address
+    ip_out = generate_ipv4_address(); /// Calls random funtion to create a random ip out address
 
     
-	// Creates a random clock cycle
+	/// Creates a random clock cycle
 	std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(2, 500);
@@ -19,7 +19,7 @@ Request::Request(){
 
 
 
-// Randomized IP Address generator function created by ChatGPT
+/// Randomized IP Address generator function created by ChatGPT
 std::string Request::generate_ipv4_address() {
   std::random_device rd;
   std::mt19937 gen(rd());
